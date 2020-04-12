bdf6660ed4fb96681b8b4870fb5a2f4a56f08ecc
aman-bansal <bansalaman2905@gmail.com>
2020-04-12 14:07:15 +0530

    fixing proto generation script

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+#docker run --rm -v $(pwd):$(pwd) -w $(pwd)/proto_files znly/protoc:0.2.0 --grpc-gateway_out=logtostderr=true:$GOPATH/src/. --go_out=plugins=grpc:$GOPATH/src/. -I$GOPATH/src/gitlab.com/machodev/user_authorization_manager/pkg/proto_files  user_manager.proto
 for proto in `ls $GOPATH/src/github.com/aman-bansal/approval-queue/pkg/proto | tr -s '\n'`
 do
     echo "$proto"
-    docker run --rm -v $(pwd):$(pwd) -w $(pwd)/proto_files znly/protoc:0.2.0 --grpc-gateway_out=logtostderr=true:$GOPATH/src/. --go_out=plugins=grpc:$GOPATH/src/. -I$GOPATH/src/github.com/aman-bansal/approval-queue/pkg/proto $proto
+    docker run --rm -v $GOPATH/src/github.com/aman-bansal/approval-queue/pkg:$GOPATH/src/github.com/aman-bansal/approval-queue/pkg -w $GOPATH/src/github.com/aman-bansal/approval-queue/pkg/proto znly/protoc:0.2.0 --grpc-gateway_out=logtostderr=true:$GOPATH/src/. --go_out=plugins=grpc:$GOPATH/src/. -I$GOPATH/src/github.com/aman-bansal/approval-queue/pkg/proto $proto
 done
